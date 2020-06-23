2875caaea6b715138f78f952086bbd67a0561693
JuanDRosero <43081291+JuanDRosero@users.noreply.github.com>
2020-06-23 13:25:16 -0500

    Ejercicio 3 resubido

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,8 +7,8 @@ Created on Tue Jun 23 13:04:33 2020
 
 def retornar(lista):
     if len(lista)==1:
-        return [[max(lista[0])]+[min(lista[0])]]
+        return [[max(lista[0])]+[min(lista[0])]+[max(lista[0])+min(lista[0])]]
                      
-    return [[max(lista[0])]+[min(lista[0])]] +retornar(lista[1:])
+    return [[max(lista[0])]+[min(lista[0])]+[max(lista[0])+min(lista[0])]] +retornar(lista[1:])
 
 print(retornar([[1,5,33],[154,3,5],[58,7,9],[5,10,12]]))
